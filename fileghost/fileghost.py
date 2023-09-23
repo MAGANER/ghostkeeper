6e95a99bfd5eec7246ee62698cc2b650197d2c11
MAGANER <maganer.krik@mail.ru>
2023-09-23 15:53:17 +0300

    refactoring

--- fileghost/fileghost.py
+++ fileghost/fileghost.py
@@ -14,13 +14,6 @@ SEP = '__FIL3GH0ST__'.encode()
 SEPARATOR = list(bytes(SEP))
 
 
-#This class can be splitted into 3 blocks
-#Here is the list of marks:
-#(just look fri @i and you will get to the right place)
-#@1 Key convertion block
-#@2 Encryption block
-#@3 Decryption block
-
 class keygen:
     @classmethod
     def generate(cls) -> "keygen":
@@ -40,55 +33,16 @@ class keygen:
 
         return cls(keys)
 
-    @classmethod
-    def from_file(cls, path: str) -> "keygen":
-        '''
-        Loads a keygen object from a file.
-        '''
-        with open(path, "r") as f:
-            keys = json.loads(f.read())
-
-        return cls(list(keys.values()))
-
     def __init__(self, keys: list) -> None:
         if set(keys) != set(range(256)):
             raise ValueError("Keystore must contain all numbers from 0 to 255")
 
         self._keys = keys
         self._keystore = dict(enumerate(self._keys))
-
-#@1 Key convertion block ###        
-    def to_keystore(self) -> dict:
-        return self._keystore.copy()
-
-    def to_byte_array(self) -> list:
-        return self._keys.copy()
-
+        
     def to_hex(self):
         return bytes(self._keys).hex()
-
-    def to_int(self):
-        return int(self.to_hex(), 16)
-
-    def to_file(self, path: str) -> None:
-        def write_keys() -> None:
-            j = json.dumps(self.to_keystore())
-            with open(path, "w") as f:
-                f.write(j)
-
-            print("Generated new keystore:", path)
-
-        if os.path.exists(path):
-            q = input('''This file already exists on specified path. Do you want to replace it?\nBe careful, if you replace it, all the files encrypted with that keystore will be lost forever.\nContinue? [\033[1mY\033[0m/\033[1mN\033[0m] ''')
-            if q not in "yY":
-                print("Aborted.")
-                sys.exit()
-
-        # path doesn't exist OR user wants to replace it
-        write_keys()
-###
         
-#@2 Encryption block ###
     def __pan(self, inp: bytes) -> bytes:
         '''if input is too short, then fill it with random bytes'''
         inp = list(inp)
@@ -105,9 +59,12 @@ class keygen:
 
 
     def encrypt(self, inp: bytes) -> bytes:
+        '''split input message -> encrypt every chunk -> concatenate encrypted chunks'''
         encrypted_chunks = [self.__encrypt_chunk(chunk) for chunk in self.__chunks(inp,256)]
         return bytes(reduce(lambda a,b: a+b, encrypted_chunks))
     def __encrypt_chunk(self, inp: bytes) -> list:
+        '''encrypt message which size is less than 256(if it's less than 256 message will be filled with salt'''
+        
         # Input is too short. Extend it with salt and random bytes.
         if len(inp) < 256:
             inp = self.__pan(inp)
@@ -120,11 +77,11 @@ class keygen:
 
         return enc_bytes
 
-    def encrypt_file(self, path: str, disable_input_max_length=False) -> list:
+    def encrypt_file(self, path: str) -> list:
         with open(path, "rb") as f:
-            return self.encrypt(f.read(), disable_input_max_length)
-###
-#@3 Decryption block ###
+            return self.encrypt(f.read())
+
+
     def decrypt(self, inp:bytes) -> bytes:
         decrypted_chunks = [self.__decrypt_chunk(chunk) for chunk in self.__chunks(inp,256)]
         return bytes(reduce(lambda a,b: a+b,decrypted_chunks))
@@ -150,4 +107,3 @@ class keygen:
     def decrypt_file(self, path: str) -> bytes:
         with open(path, "rb") as f:
             return self.decrypt(f.read())
-###
